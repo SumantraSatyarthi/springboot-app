2fe6e6fd74eaf0a94d3cb52585f17ec9b8409158
SumantraSatyarthi <sumantra.satyarthi4@gmail.com>
2025-04-16 07:08:55 +0530

    first commit

---
+++ src/main/java/net/javagides/springboot_app/HelloCotroller.java
@@ -0,0 +1,12 @@
+package net.javagides.springboot_app;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloCotroller {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "Hello Bobby";
+    }       
+}
